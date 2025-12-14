516cc36e03c1ab1350820a23eb0160d9a37ebd81
CrispyMiatr <maxivowes@gmail.com>
2025-12-14 19:11:06 +0100

    feat(RangeSlider): show info on 3s hover + tweak styling

--- src/modules/components/ui/RangeSlider.tsx
+++ src/modules/components/ui/RangeSlider.tsx
@@ -4,9 +4,14 @@ import type { RangeSliderType } from "~/shared";
 export const RangeSlider = ({ value, min = 0, max = 10, step = 0.1, label, onChange }: RangeSliderType) => {
     return (
         <div className="range-slider">
+            <p className="range-slider__info">
+                Try speed 4.2!
+            </p>
+
             <label htmlFor="range-input" className="range-slider__label">
                 {label} <span className="range-slider__value">×{value.toFixed(1)}</span>
             </label>
+
             <input
                 id="range-input"
                 className="range-slider__input"
