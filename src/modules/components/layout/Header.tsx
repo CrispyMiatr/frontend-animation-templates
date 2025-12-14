a58afb24181c1de98182b4d563f9d24c8ec19f47
CrispyMiatr <maxivowes@gmail.com>
2025-12-14 19:13:21 +0100

    fix(Header): change homepage name to Puzzle

--- src/modules/components/layout/Header.tsx
+++ src/modules/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import { NavButton } from '../ui';
 export const Header = () => {
     return (
         <nav className={styles['navbar']}>
-            <NavButton name='Home' variant='btn0' link='/' />
+            <NavButton name='Puzzle' variant='btn0' link='/' />
             <NavButton name='Assets' variant='btn0' link='/assets' />
         </nav>
     );
